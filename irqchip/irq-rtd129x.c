c10e7f10ee3cf54453992b6b6544392c61c87b2d
Celliwig <celliwig@nym.hush.com>
2026-03-21 22:32:43 +0000

    Added Realtek clock gate driver

--- irqchip/irq-rtd129x.c
+++ irqchip/irq-rtd129x.c
@@ -7,6 +7,8 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
+ *
+ * Updated 2026 - Celliwig <celliwig@nym.hush.com>
  */
 
 #include <linux/err.h>
